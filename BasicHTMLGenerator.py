5265d7a7d3f561b911721fe1e9a2a47ef9ca7967
Bharath Santosh <bsant99@gmail.com>
2013-10-06 04:36:38 -0400

    Added css tree decorator

--- BasicHTMLGenerator.py
+++ BasicHTMLGenerator.py
@@ -7,7 +7,7 @@ class BasicHTMLGenerator(HTMLGenerator):
         pass
 
     def Generate(self,htmlTree):
-        body = "<body>%s</body>" % str(htmlTree)
+        body = "<body><div style='margin: auto; border: dashed; width: %dpx; height: %dpx'>%s</div></body>" % (htmlTree.rootBodyNode.boundingBox.width, htmlTree.rootBodyNode.boundingBox.height, str(htmlTree))
         html = "<!DOCTYPE html><html lang='en'><head><title>A First Draft</title></head>%s</html>" % body 
         return html
 
