481eaed42f74ff7f802e932d50e83eef2906ba68
Ted Meyer <tmathmeyer@gmail.com>
2013-10-05 21:01:15 -0700

    testing redis

--- redis-test.py
+++ redis-test.py
@@ -1,12 +1,37 @@
 import redis
 
-def test():
-    r = redis.StrictRedis(host='localhost', port=6379, db=0)
-    k = r.get('test');
-    z = int(k)
-    print k
-    print z
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+def insertElem(a):
+    counter = getCounter()
+    r.put(counter+1, a);
+
+def test():
+    for x in range(0, 37):
+        insertElem(x)
+    print_range(0, 10);
+
+def getCounter():
+	counter = r.get('counter')
+	if counter equals 'NoneType':
+	    return int(counter)
+	return 0
+
+
+def printNewest():
+    counter = getCounter()
+    print print_range(counter, counter-10)
+
+def print_range(a, b):
+    result = "<html><body>"
+    for x in range (a, b):
+    	result+=get_html_for(x)
+    return result+="</body></html>"
+
+def get_html_for(a):
+	if not a equals 'NoneType':
+	    return "<img src = \""+a+"\" height=\"50px\" width=\"50px\"><div>"+a+"</div><br />"
+	return ""
 
 
 if __name__ == '__main__':
