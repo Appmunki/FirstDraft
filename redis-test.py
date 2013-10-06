e35c64947e943beb199e3426a301d8f1f2a28c43
Ted Meyer <tmathmeyer@gmail.com>
2013-10-05 20:41:42 -0400

    time to merge with the server

--- redis-test.py
+++ redis-test.py
@@ -4,32 +4,37 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 def insertElem(a):
     counter = getCounter()
-    r.put(counter+1, a);
+    r.delete('counter')
+    r.set('counter', counter+1)
+    r.set(counter, a);
 
 def test():
+    r.delete('counter')
     for x in range(0, 37):
         insertElem(x)
-    print_range(0, 10);
+    print printNewest(10)
 
 def getCounter():
 	counter = r.get('counter')
-	if counter equals 'NoneType':
+	if counter:
 	    return int(counter)
 	return 0
 
-
-def printNewest():
+def printNewest(anum):
     counter = getCounter()
-    print print_range(counter, counter-10)
+    return print_range(counter-1-anum, counter-1)
+ 
 
 def print_range(a, b):
     result = "<html><body>"
     for x in range (a, b):
-    	result+=get_html_for(x)
-    return result+="</body></html>"
+    	temp=get_html_for(r.get(x))
+	result+=temp
+    result+="</body></html>"
+    return result
 
 def get_html_for(a):
-	if not a equals 'NoneType':
+	if a:
 	    return "<img src = \""+a+"\" height=\"50px\" width=\"50px\"><div>"+a+"</div><br />"
 	return ""
 
